1dfb86cbd8c74ac34eaa542ae76997347b996b58
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-09-14 19:18:55 -0300

    Criação do servidor no app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__) #A variável app recebe o objeto Flask, essa parte sinaliza que vou usar o flask no meu arquivo principal.
+
+#Rota HOME, com uma função que não recebe parametro e renderiza o index.html no retorno dela.
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run() #Vai rodar o aqruvio se ele tiver no arquivo principal, ou seja, na main.
