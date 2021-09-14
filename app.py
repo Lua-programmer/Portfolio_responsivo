08bddaaf7c0b09681e21f72299a2f1fec3bd891e
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-09-14 19:56:50 -0300

    Configuração do ambiente com o bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect
 
-app = Flask(__name__) #A variável app recebe o objeto Flask, essa parte sinaliza que vou usar o flask no meu arquivo principal.
+app = Flask(__name__) 
+#A variável app recebe o objeto Flask, essa parte sinaliza que vou usar o flask no meu arquivo principal.
 
 #Rota HOME, com uma função que não recebe parametro e renderiza o index.html no retorno dela.
 @app.route('/')
@@ -8,4 +9,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run() #Vai rodar o aqruvio se ele tiver no arquivo principal, ou seja, na main.
+    app.run() 
+    #Vai rodar o arquivo se ele tiver no arquivo principal, ou seja, na main. O debug ativa o modo desenvolvedor e vai fazer com que minha página atualize automaticamente.
